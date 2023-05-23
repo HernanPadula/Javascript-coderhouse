92e8435b0bf3b07d4182519a52c6866752473f27
hernan padula <hernanpadula@gmail.com>
2023-05-23 15:37:26 -0300

    Primera preentrega coderhouse

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function CalcularImpuestos(precioInicial){
+    let precioGan = precioInicial * 0.45;
+    let impPais = precioInicial * 0.30;
+    let precioTotal = precioGan + impPais + precioInicial;
+    alert("Precio original: $"+precioInicial+"\nImpuesto pais: $"+impPais+"\nImpuesto a las ganancias: $"+precioGan+"\nPrecio total: $"+precioTotal);
+}
+
+let precioJuego = parseFloat(prompt('ingresa el precio de tu juego en pesos (sin impuestos)'));
+
+CalcularImpuestos(precioJuego);
+
