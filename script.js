e490c4f64c13b14d487b8d5fbf39d85d8d54780e
hernan padula <hernanpadula@gmail.com>
2023-06-13 17:47:52 -0300

    correcion 2

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// function CalcularImpuestos(precioInicial){
-//     let precioGan = precioInicial * 0.45;
-//     let impPais = precioInicial * 0.30;
-//     let precioTotal = precioGan + impPais + precioInicial;
-//     alert("Precio original: $"+precioInicial+"\nImpuesto pais: $"+impPais+"\nImpuesto a las ganancias: $"+precioGan+"\nPrecio total: $"+precioTotal);
-// }
-
-// while(CalcularImpuestos){
-// let precioJuego = parseFloat(prompt('ingresa el precio de tu juego en pesos (sin impuestos)'));
-
-// if(precioJuego > 0){
-// CalcularImpuestos(precioJuego);
-// }else{
-//     alert("ingrese un precio valido");
-// }
-// }
-
 const juegos = [
     {
         nombre: "SIFU",
