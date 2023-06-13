c07a8a1b4b0eab517920d455e9a9396510c859b6
hernan padula <hernanpadula@gmail.com>
2023-06-13 17:44:56 -0300

    creacion de filtrado por precio

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
+// function CalcularImpuestos(precioInicial){
+//     let precioGan = precioInicial * 0.45;
+//     let impPais = precioInicial * 0.30;
+//     let precioTotal = precioGan + impPais + precioInicial;
+//     alert("Precio original: $"+precioInicial+"\nImpuesto pais: $"+impPais+"\nImpuesto a las ganancias: $"+precioGan+"\nPrecio total: $"+precioTotal);
+// }
 
-function CalcularImpuestos(precioInicial){
-    let precioGan = precioInicial * 0.45;
-    let impPais = precioInicial * 0.30;
-    let precioTotal = precioGan + impPais + precioInicial;
-    alert("Precio original: $"+precioInicial+"\nImpuesto pais: $"+impPais+"\nImpuesto a las ganancias: $"+precioGan+"\nPrecio total: $"+precioTotal);
-}
+// while(CalcularImpuestos){
+// let precioJuego = parseFloat(prompt('ingresa el precio de tu juego en pesos (sin impuestos)'));
 
-while(CalcularImpuestos){
-let precioJuego = parseFloat(prompt('ingresa el precio de tu juego en pesos (sin impuestos)'));
+// if(precioJuego > 0){
+// CalcularImpuestos(precioJuego);
+// }else{
+//     alert("ingrese un precio valido");
+// }
+// }
 
-if(precioJuego > 0){
-CalcularImpuestos(precioJuego);
-}else{
-    alert("ingrese un precio valido");
-}
-}
+const juegos = [
+    {
+        nombre: "SIFU",
+        precio: 3099
+    },
+    {
+        nombre: "PROJECT ZOMBOID",
+        precio: 280
+    },
+    {
+        nombre: "RESIDENT EVIL 4",
+        precio: 12999
+    },
+    {
+        nombre: "OUTLAST TRIALS",
+        precio: 2599
+    },
+    {
+        nombre: "BULLY",
+        precio: 300
+    },
+    {
+        nombre: "GROUNDED",
+        precio: 1599
+    },
+    {
+        nombre: "COUNTER-STRIKE GLOBAL OFFENSIVE",
+        precio: 3219
+    }
+
+]
+
